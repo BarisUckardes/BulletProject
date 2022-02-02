2ca691c49e15615ca19dc8763a972d9e68d1ccfb
Roveldo <baris.uckardess@gmail.com>
2022-02-03 01:37:20 +0300

    Add project files.

---
+++ BulletProject/src/EntryPoint.cpp
@@ -0,0 +1,10 @@
+
+#include <GL/freeglut.h>
+
+int main(int argumentCount, char** arguments)
+{
+	glutInit(&argumentCount, arguments);
+	//glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+
+	return 0;
+}
